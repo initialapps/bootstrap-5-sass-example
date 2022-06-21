b7c0fb068efeefd527e2d2050c01ca6b8f29cada
Andrew Akagawa <andrewakagawa@gmail.com>
2022-06-20 23:22:00 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <h1>Bootstrap Buttons!</h1>
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+        <button type="button" class="btn btn-warning">Warning</button>
+        <button type="button" class="btn btn-info">Info</button>
+      </div>
+    </div>
+    <h1>Custom Buttons!</h1>
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-dark-lava">Dark Lava</button>
+        <button type="button" class="btn btn-goldenrod">Goldenrod</button>
+        <button type="button" class="btn btn-alabaster">Alabaster</button>
+        <button type="button" class="btn btn-spanish-gray">Spanish Gray</button>
+        <button type="button" class="btn btn-dim-gray">Dim Gray</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss">
+  @import "@/assets/sass/main.scss";
+</style>
